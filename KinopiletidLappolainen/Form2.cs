82a78663eeda42579a5be327fe16980a21df1bfd
opilane <opilane@HP-CZC6297B8F>
2020-09-16 11:34:52 +0300

    pochti sdelano no ne rabotaet fig znaet pochemu

--- KinopiletidLappolainen/Form2.cs
+++ KinopiletidLappolainen/Form2.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Globalization;
 
 namespace KinopiletidLappolainen
 {
@@ -19,21 +20,36 @@ namespace KinopiletidLappolainen
         {
             InitializeComponent();
         }
+        StreamWriter to_file;
         Image green = Image.FromFile("Image/green.png");
         Image yellow = Image.FromFile("Image/yellow.png");
         Image red = Image.FromFile("Image/red.png");
         Label[,] _arr = new Label[4, 4];
         Label[] read = new Label[4];
         Button osta;
+        Button kinni;
+        bool ost = false;
         private void Form2_Load(object sender, EventArgs e)
         {
 
-   
-
-            string text;
+            string text = "";
             if (!File.Exists("Kino.txt"))
             {
-                StreamWriter to_file1 = new StreamWriter("Kino.txt", false);            
+                StreamWriter to_file = new StreamWriter("Kino.txt", false);
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        text += i + "," + j + ",false;";
+                    }
+                    text += "\n";
+                }
+                to_file.Write(text);
+                to_file.Close();
+            }
+                StreamReader from_file = new StreamReader("Kino.txt", false);
+                string[] arr = from_file.ReadToEnd().Split('\n');
+                from_file.Close();
                 for (int i = 0; i < 4; i++)
                 {
                     read[i] = new Label();
@@ -46,11 +62,21 @@ namespace KinopiletidLappolainen
                     for (int j = 0; j < 4; j++)
                     {
                         _arr[i, j] = new Label();
+                        string[] arv = arr[i].Split(';');
+                        string[] ardNum = arv[j].Split(',');
+                        if(ardNum[2] == "true")
+                        {
+                            _arr[i, j].Image = red;
+                        }
+                        else
+                        {
+                            _arr[i, j].Image = green;
+                        }
                         //_arr[i, j].Text = "Koht " + (j + 1); 
                         _arr[i, j].Size = new Size(50, 50);
                         _arr[i, j].Image = green;
                         _arr[i, j].BackColor = Color.Transparent;
-                        //_arr[i, j].BorderStyle = BorderStyle.Fixed3D;
+                       // _arr[i, j].BorderStyle = BorderStyle.Fixed3D;
                         _arr[i, j].Location = new Point(j * 50 + 300, i * 50 + 100);
                         this.Controls.Add(_arr[i, j]);
                         _arr[i, j].Tag = new int[] { i, j };
@@ -60,48 +86,100 @@ namespace KinopiletidLappolainen
                 }
                 osta = new Button();
                 osta.Text = "Osta";
-                osta.Location = new Point(350, 350);
+                osta.Location = new Point(325, 350);
                 this.Controls.Add(osta);
                 osta.Click += Osta_Click;
+                kinni = new Button();
+                kinni.Text = "Kinni";
+                kinni.Location = new Point(415, 350);
+                this.Controls.Add(kinni);
+                kinni.Click += Kinni_Click;
+                
+                
+            
+        }private void Osta_Click(object sender, EventArgs e)
+        {
+            Osta_Click_Func();
+            ost = true;
+            
+        }
+
+        private void Osta_Click_Func()
+        {
+            if(ost == true)
+            {
+                var vastus = MessageBox.Show("Kas oled kindel?", "Appolo küsib", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (vastus == DialogResult.Yes)
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        for (int j = 0; j < 4; j++)
+                        {
+                            if (_arr[i, j].Image == yellow)
+                            {
+                                _arr[i, j].Image = red;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        for (int j = 0; j < 4; j++)
+                        {
+                            if (_arr[i, j].Image == yellow)
+                            {
+                                _arr[i, j].Image = green;
+                            }
+                        }
+                    }
+                }
             }
         }
 
-        private void Osta_Click(object sender, EventArgs e)
+        private void Kinni_Click(object sender, EventArgs e)
         {
-            var vastus = MessageBox.Show("Kas oled kindel?", "Appolo küsib", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-            if (vastus == DialogResult.Yes)
+            string text = " ";
+            to_file = new StreamWriter("Kino.txt", false);
+            for (int i = 0; i<4; i++)
             {
-                for (int i = 0; i < 4; i++)
+                for(int j = 0; j < 4; j++)
                 {
-                    for (int j = 0; j < 4; j++)
+                    if(_arr[i,j].Image == yellow)
                     {
-                        if (_arr[i, j].Image == yellow)
-                        {
-                            _arr[i, j].Image = red;
-                        }
+                        Osta_Click_Func();
                     }
                 }
             }
-            else
+            for(int i = 0; i< 4; i++)
             {
-                for (int i = 0; i < 4; i++)
+                for(int j = 0; j < 4; j++)
                 {
-                    for (int j = 0; j < 4; j++)
+                    if(_arr[i,j].Image == red)
                     {
-                        if (_arr[i, j].Image == yellow)
-                        {
-                            _arr[i, j].Image = green;
-                        }
+                        text += i + "," + j + ",true;";
+                    }
+                    else
+                    {
+                        text += i + "," + j + ",false;";
                     }
                 }
+                text += "\n";
+                
             }
+            to_file.Write(text);
+            to_file.Close();
+            this.Close();
         }
+
+        
 
         private void Form2_Click(object sender, EventArgs e)
         {
             var label = (Label)sender;
             var tag = (int[])label.Tag;
-            //_arr[tag[0], tag[1]].Text = "Kinni";
+            _arr[tag[0], tag[1]].Text = "Kinni";
            if( _arr[tag[0], tag[1]].Image == red)
             {
                 MessageBox.Show("See koht on kinni");
